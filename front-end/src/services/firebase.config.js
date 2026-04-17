8e4e6d0c53026a7e4cae1b8d25196b9a2d026390
tranduyfong <tranphong86958@gmail.com>
2026-04-17 22:52:49 +0700

    Remove something

--- front-end/src/services/firebase.config.js
+++ front-end/src/services/firebase.config.js
@@ -1,26 +1,15 @@
 import { initializeApp } from 'firebase/app';
 import { getStorage } from 'firebase/storage';
 
-// const firebaseConfig = {
-//     apiKey: import.meta.env.API_KEY_FIREBASE,
-//     authDomain: import.meta.env.AUTH_DOMAIN_FIREBASE,
-//     projectId: import.meta.env.PROJECT_ID_FIREBASE,
-//     storageBucket: import.meta.env.STORAGE_BUCKET_FIREBASE,
-//     messagingSenderId: import.meta.env.MESSAGING_SENDER_ID,
-//     appId: import.meta.env.APP_ID_FIREBASE,
-//     measurementId: import.meta.env.MEASUREMENT_ID
-// }
-
 const firebaseConfig = {
-    apiKey: "AIzaSyBjdJL_q8rdcdbolBM38NfdQ9WF6_QKcMk",
-    authDomain: "fir-storage-image-98000.firebaseapp.com",
-    projectId: "fir-storage-image-98000",
-    storageBucket: "fir-storage-image-98000.firebasestorage.app",
-    messagingSenderId: "130838738902",
-    appId: "1:130838738902:web:039915cc2cc964c6135677",
-    measurementId: "G-LJYPR9KQ6X"
-};
-
+    apiKey: import.meta.env.API_KEY_FIREBASE,
+    authDomain: import.meta.env.AUTH_DOMAIN_FIREBASE,
+    projectId: import.meta.env.PROJECT_ID_FIREBASE,
+    storageBucket: import.meta.env.STORAGE_BUCKET_FIREBASE,
+    messagingSenderId: import.meta.env.MESSAGING_SENDER_ID,
+    appId: import.meta.env.APP_ID_FIREBASE,
+    measurementId: import.meta.env.MEASUREMENT_ID
+}
 
 const app = initializeApp(firebaseConfig);
 export const storage = getStorage(app);
